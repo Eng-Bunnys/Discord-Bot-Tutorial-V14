2922ebe7f70809400cfc9f34991627ee8e8c70ef
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2023-06-27 19:39:12 +0300

    Ep1

--- Episode 1 & 2/Episode 1/index.js
+++ Episode 1 & 2/Episode 1/index.js
@@ -2,11 +2,18 @@ const { Client, GatewayIntentBits, Events } = require('discord.js');
 const { TOKEN } = require('./config/GBFconfig.json');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
 
 client.on(Events.ClientReady, (client) => {
   console.log(`${client.user.username} is now online`);
+});
+
+client.on(Events.MessageCreate, async (client) => {
+  if (message.content.toLowerCase() === 'ping') {
+    return message.reply({
+      content: `Pong!`
+    });
 });
 
 client.login(TOKEN);
